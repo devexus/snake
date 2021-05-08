49a06d2e5a6521666aa7d1e442ad3260911d0bd3
koszczepanski <47736308+koszczepanski@users.noreply.github.com>
2021-05-08 16:25:30 +0200

    build

--- src/helpers/helpers.ts
+++
@@ -1,9 +0,0 @@
-export const generatePosition = (divider: number, min: number, max: number) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  let i = -1;
-  do {
-    i = Math.floor(Math.random() * (max - min)) + min;
-  } while (i % divider);
-  return i;
-};
